3a2721b2aaf807d79c8f92b91fa69c5aa72c9f80
Alexsandrrh <sadovspbsasha@outlook.com>
2018-07-22 13:09:06 +0300

    update

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -63,6 +63,7 @@ $(document).ready(function () {
                 }
 
                 $('.js-price-for').val('');
+                params.removeClass('-active');
             });
 
             // search
